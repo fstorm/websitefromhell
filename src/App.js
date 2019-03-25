8fcb7e132f2075442f7a97e702772a2bb5a3c6c2
Felix Storm-Jensen <felix.storm-jensen@zuhlke.com>
2019-03-25 13:41:47 +0800

    Added ejected files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import BadForm from './BadForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Create an account</h1>
+        <BadForm />
       </div>
     );
   }
---
+++ src/BadForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const badForm = () => {
+    return (
+        <div className="bad-form">
+            <label>Name</label>
+            <input />
+        </div>
+    )
+};
+
+export default badForm;
